6013bcc41d57557deb0e4be1ce127fdffce2ef1c
Rémi Weng <ixartz@users.noreply.github.com>
2021-12-01 03:09:42 +0100

    Install ripgrep with brew sh

--- macos/install.sh
+++ macos/install.sh
@@ -5,7 +5,7 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install brew application
-brew install gh	nvm trash tree httpie jq htop antigen fzf
+brew install gh nvm trash tree httpie jq htop antigen fzf ripgrep
 
 # Install Vim-plug manually install. Then, in Vim, run :PlugInstall
 
